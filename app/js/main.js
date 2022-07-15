bb0d67fd339ff0b191f036b7f1fe45c955a338e4
slavik <slavik.zaliotnyi@gmail.com>
2022-07-15 13:46:04 +0300

    added popup

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
 $(function () {
-
+  $('button').on('click', function () {
+    $('.js-overlay-campaign').fadeIn();
+    $('body').addClass('lock');
+  });
+  $('.close__popup').on('click', function () {
+    $('.js-overlay-campaign').fadeOut();
+    $('body').removeClass('lock');
+  });
 })
